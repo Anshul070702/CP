e908e356e274c2e771f658f473ab002ed8a300f0
Anshul <0069ansh.ul@gmail.com>
2024-03-04 23:55:58 +0530

    Important Function

---
+++ Leetcode/Important.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+/*  For Ordered_Set  */
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define godspeed                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+/* 2-D Matrix Prefix Sum */
+vector<vector<int>> PrefixSum(vector<vector<int>> &mat)
+{
+    int n = mat.size(), m = mat[0].size();
+    vector<vector<int>> pre(n + 1, vector<int>(m + 1, 0));
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            pre[i][j] = mat[i - 1][j - 1] + pre[i - 1][j] + pre[i][j - 1] - pre[i - 1][j - 1];
+        }
+    }
+}
+
+int main()
+{
+    godspeed;
+    return 0;
+}
